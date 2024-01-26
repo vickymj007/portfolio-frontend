6d83b27ffa5a961f3ac119acba6f3b2c01d9853e
Vicky <vickypersish1997@gmail.com>
2024-01-26 09:50:21 +0530

    Updated resume and changed the Profile Image size

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -11,7 +11,7 @@ const Intro = () => {
             <span className='hello'>Hello,</span>
             <span className='intro_text'>I'm <span className='intro_name'>Vignesh</span><br/>MERN Stack Developer</span>
             <p className='intro_para'>Iam a skilled Web Developer with experience in creating <br/> visually appealing and user friendly websites</p>
-            <a href="https://docs.google.com/document/d/1Cb-mMTK8EsZvBEQ5WmjZpqidwHYtvl08rRL-sKLheTk/edit?usp=sharing" target='blank' className="resume_btn">View Resume</a>
+            <a href="https://drive.google.com/file/d/1s4ERHui6APTH1jt7Y7Vc3KhSw6NBVLib/view?usp=sharing" target='blank' className="resume_btn">View Resume</a>
         </div>
         <img className='bg' src={mypic} alt="My cover" />
     </section>
